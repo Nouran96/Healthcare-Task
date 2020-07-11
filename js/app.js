92cb1969da4f737cf9782d6276de37dce8c3591e
Nouran Samy <nourans.96@yahoo.com>
2020-07-11 17:26:05 +0200

    Validate and store data from form

--- js/app.js
+++ js/app.js
@@ -10,95 +10,55 @@ function showShoulders() {
     document.querySelector('#confirm').style.display = 'block';
 }
 
-// function closeDiagram() {
-//     window.history.back();
-// }
+let form1 = document.forms["form-1"];
+let form2 = document.forms["form-2"];
+let form3 = document.forms["form-3"];
 
-var ctx = document.getElementById('myChart').getContext('2d');
-// ctx.moveTo(0, 0);
-// ctx.lineTo(200, 100);
-// ctx.stroke();
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["<2 weeks(n=33)", '2–4 weeks(n=36)', '5–8 weeks(n=42)', '9–12 weeks(n=62)', '>12 weeks(n=16)'],
-        datasets: [{
-            data: [1, 2, 4, 10, 6],
-            backgroundColor: [
-                'white',
-                'white',
-                'white',
-                'white',
-                'white',
-            ],
-            borderColor: [
-                '#23285A',
-                '#23285A',
-                '#23285A',
-                '#23285A',
-                '#555658',
-            ],
-            borderWidth: 15
-        }]
-    },
-    options: {
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    color: "rgba(0, 0, 0, 0)",
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true,
-                    stepSize: 2,
-                    suggestedMax: 12
-                },
-                gridLines: {
-                    color: "rgba(0, 0, 0, 0)",
-                },
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Odds ratio'
-                }
-            }]
-        },
-        responsive: true,
-        legend: {
-            display: false,
-        },
-        annotation: {
-            annotations: [{
-              type: 'line',
-              mode: 'horizontal',
-              scaleID: 'y-axis-0',
-              value: 1,
-              borderColor: '#555658',
-              borderWidth: 1,
-              label: {
-                enabled: false,
-                content: 'Test label'
-              }
-            }]
+let daysMissed = document.querySelector("#daysMissed");
+
+var event = new Event('change');
+
+if(form1) {
+    form1.addEventListener('submit', function(e) {
+        e.preventDefault();
+        localStorage.setItem("intensity", form1["intensity"].value);
+        localStorage.setItem("frequency", form1["frequency"].value);
+        localStorage.setItem("frequency-desc", form1["frequency-desc"].value);
+        window.location.href="form-2.html"
+    });
+}
+
+if(form2) {
+
+    form2["takingMedicine"].value = localStorage.getItem("takingMedicine");
+    form2["daysMissed"].value = localStorage.getItem("daysMissed");
+
+    daysMissed.addEventListener("change", () => {
+        form2.dispatchEvent(event);
+    });
+
+    form2.addEventListener("change", () => {
+        if(localStorage.getItem("daysMissed") != null &&
+            localStorage.getItem("painType") != null &&
+            localStorage.getItem("takingMedicine") != null) {
+
+            document.querySelector("#continueBtn").removeAttribute("disabled");
         }
-    }
-});
+    });
 
-// ctx.beginPath();
-// canvas_arrow(ctx, 10, 30, 200, 150);
-// canvas_arrow(ctx, 100, 200, 400, 50);
-// canvas_arrow(ctx, 200, 30, 10, 150);
-// canvas_arrow(ctx, 400, 200, 100, 50);
-// ctx.stroke();
+    form2.addEventListener('submit', function(e) {
+        e.preventDefault();
+        localStorage.setItem("takingMedicine", form2["takingMedicine"].value);
+        localStorage.setItem("daysMissed", form2["daysMissed"].value);
+        window.location.href="form-3.html"
+    });
+}
 
-// function canvas_arrow(context, fromx, fromy, tox, toy) {
-//   var headlen = 10; // length of head in pixels
-//   var dx = tox - fromx;
-//   var dy = toy - fromy;
-//   var angle = Math.atan2(dy, dx);
-//   context.moveTo(fromx, fromy);
-//   context.lineTo(tox, toy);
-//   context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
-//   context.moveTo(tox, toy);
-//   context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
-// }
+if(form3) {
+
+    form3.addEventListener('submit', function(e) {
+        e.preventDefault();
+        localStorage.setItem("painType", form3["symptom"].value);
+        window.location.href="form-2.html";
+    });
+}
---
+++ js/chart.js
@@ -0,0 +1,88 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+// ctx.moveTo(0, 0);
+// ctx.lineTo(200, 100);
+// ctx.stroke();
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["<2 weeks(n=33)", '2–4 weeks(n=36)', '5–8 weeks(n=42)', '9–12 weeks(n=62)', '>12 weeks(n=16)'],
+        datasets: [{
+            data: [1, 2, 4, 10, 6],
+            backgroundColor: [
+                'white',
+                'white',
+                'white',
+                'white',
+                'white',
+            ],
+            borderColor: [
+                '#23285A',
+                '#23285A',
+                '#23285A',
+                '#23285A',
+                '#555658',
+            ],
+            borderWidth: 15
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 2,
+                    suggestedMax: 12
+                },
+                gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Odds ratio'
+                }
+            }]
+        },
+        responsive: true,
+        legend: {
+            display: false,
+        },
+        annotation: {
+            annotations: [{
+              type: 'line',
+              mode: 'horizontal',
+              scaleID: 'y-axis-0',
+              value: 1,
+              borderColor: '#555658',
+              borderWidth: 1,
+              label: {
+                enabled: false,
+                content: 'Test label'
+              }
+            }]
+        }
+    }
+});
+
+// ctx.beginPath();
+// canvas_arrow(ctx, 10, 30, 200, 150);
+// canvas_arrow(ctx, 100, 200, 400, 50);
+// canvas_arrow(ctx, 200, 30, 10, 150);
+// canvas_arrow(ctx, 400, 200, 100, 50);
+// ctx.stroke();
+
+// function canvas_arrow(context, fromx, fromy, tox, toy) {
+//   var headlen = 10; // length of head in pixels
+//   var dx = tox - fromx;
+//   var dy = toy - fromy;
+//   var angle = Math.atan2(dy, dx);
+//   context.moveTo(fromx, fromy);
+//   context.lineTo(tox, toy);
+//   context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
+//   context.moveTo(tox, toy);
+//   context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+// }
